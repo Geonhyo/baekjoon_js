deca769d53c8d0d522e556999b31941853deac73
GeonhyoPark815 <rjsgy0815@gmail.com>
2022-11-03 23:33:03 +0900

    [221103] 백준 No.10952 A+B - 5

---
+++ 10952_A+B-5/solution.js
@@ -0,0 +1,14 @@
+const input = require('fs').readFileSync('example.txt').toString().toString().trim().split('\n');
+
+let result = '';
+let sum = 0;
+
+input.map(line => {
+    line.split(' ').map(num => sum += Number(num));
+    if(sum !== 0){
+        result += `${sum}\n`;
+    }
+    sum = 0;
+})
+
+console.log(result);
